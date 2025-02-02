f118b9b225a562bea20c61608be39b858a76d327
Rostarc <147600695+Rostarc@users.noreply.github.com>
2025-02-02 15:31:17 +0200

    Update VPN-Setup-Ubuntu20.04-22.04.sh

--- VPN-Setup-Ubuntu20.04-22.04.sh
+++ VPN-Setup-Ubuntu20.04-22.04.sh
@@ -345,6 +345,7 @@ sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables-save > /etc/iptables/rules.v4
 sudo iptables-save | sudo tee /etc/iptables/rules.v4
 sudo service iptables restart
+sudo systemctl restart netfilter-persistent
 sudo rm -rf /var/www
 sudo git clone https://github.com/Rostarc/VPN-Web-Installer.git /var/www/html
 
